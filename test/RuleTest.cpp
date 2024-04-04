a45d2ba9352f4fe42f556b949d051e33535ddbfa
Adrian Schollmeyer <adrian.schollmeyer@fem.tu-ilmenau.de>
2024-04-04 18:40:16 +0200

    test: fix Landlock 4 net rule test
    Just a wrong struct member name (copy&paste error).

--- test/RuleTest.cpp
+++ test/RuleTest.cpp
@@ -95,7 +95,7 @@ TEST_CASE("Rule::NetPortRule")
 			REQUIRE(rules.size() == 1);
 			CHECK(rules.at(0).allowed_access ==
 			      expected_action.type_code());
-			CHECK(rules.at(0).parent_fd > 0);
+			CHECK(rules.at(0).port > 0);
 #else
 			CHECK(rules.empty());
 #endif
