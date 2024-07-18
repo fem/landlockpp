e6dc26f0234095a9e863903387432e00f03e31f0
Adrian Schollmeyer <adrian.schollmeyer@fem.tu-ilmenau.de>
2024-07-18 16:07:08 +0200

    test: extend rule tests up to Landlock API 4

--- test/RuleTest.cpp
+++ test/RuleTest.cpp
@@ -4,8 +4,6 @@
 
 #include "test.hpp"
 
-#include <limits>
-
 using landlock::ActionType;
 using landlock::NetPortRule;
 using landlock::PathBeneathRule;
@@ -13,50 +11,86 @@ namespace action = landlock::action;
 
 TEST_CASE("Rule::PathBeneathRule")
 {
-	PathBeneathRule rule;
+	PathBeneathRule pb_rule;
+	NetPortRule np_rule;
 
-	rule.add_action(action::FS_EXECUTE)
+	pb_rule.add_action(action::FS_EXECUTE)
 		.add_action(action::FS_MAKE_DIR)
 		.add_action(action::FS_MAKE_SOCK)
-		.add_action(action::FS_REFER);
-	rule.add_path("/bin/sh");
+		.add_action(action::FS_REFER)
+		.add_action(action::FS_TRUNCATE);
+	pb_rule.add_path("/bin/sh");
+	np_rule.add_action(action::NET_BIND_TCP);
+	np_rule.add_port(42); // NOLINT(*-magic-numbers)
 
 	SECTION("valid ABI")
 	{
-		ActionType expected_action = action::INVALID_ACTION;
+		ActionType expected_action_pb = action::INVALID_ACTION;
+		[[maybe_unused]] ActionType expected_action_np =
+			action::INVALID_ACTION;
 		int abi = 0;
-		PathBeneathRule::AttrVec rules;
 
 		SECTION("ABI 1")
 		{
-			expected_action = action::FS_EXECUTE |
-					  action::FS_MAKE_DIR |
-					  action::FS_MAKE_SOCK;
+			expected_action_pb = action::FS_EXECUTE |
+					     action::FS_MAKE_DIR |
+					     action::FS_MAKE_SOCK;
 			abi = 1;
 		}
 
 		SECTION("ABI 2")
 		{
-			expected_action =
+			expected_action_pb =
 				action::FS_EXECUTE | action::FS_MAKE_DIR |
 				action::FS_MAKE_SOCK | action::FS_REFER;
 			abi = 2;
 		}
 
-		rules = rule.generate(abi);
+		SECTION("ABI 3")
+		{
+			expected_action_pb =
+				action::FS_EXECUTE | action::FS_MAKE_DIR |
+				action::FS_MAKE_SOCK | action::FS_REFER |
+				action::FS_TRUNCATE;
+			abi = 3;
+		}
+
+		SECTION("ABI 4")
+		{
+			expected_action_pb =
+				action::FS_EXECUTE | action::FS_MAKE_DIR |
+				action::FS_MAKE_SOCK | action::FS_REFER |
+				action::FS_TRUNCATE;
+			abi = 4;
+		}
+
+		const PathBeneathRule::AttrVec pb_rules = pb_rule.generate(abi);
+		const NetPortRule::AttrVec np_rules = np_rule.generate(abi);
 
 		if (abi > 0) {
-			REQUIRE(rules.size() == 1);
-			CHECK(rules.at(0).allowed_access ==
-			      expected_action.type_code());
-			CHECK(rules.at(0).parent_fd > 0);
+			REQUIRE(pb_rules.size() == 1);
+			CHECK(pb_rules.at(0).allowed_access ==
+			      expected_action_pb.type_code());
+			CHECK(pb_rules.at(0).parent_fd > 0);
 		}
+
+#if LLPP_BUILD_LANDLOCK_API >= 4
+		if (abi >= 4) {
+			REQUIRE(np_rules.size() == 1);
+			CHECK(np_rules.at(0).allowed_access ==
+			      expected_action_np.type_code());
+			// NOLINTNEXTLINE(*-magic-numbers)
+			CHECK(np_rules.at(0).port == 42);
+		}
+#endif
 	}
 
 	SECTION("invalid ABI")
 	{
-		const PathBeneathRule::AttrVec rules = rule.generate(0);
-		CHECK(rules.empty());
+		const PathBeneathRule::AttrVec pb_rules = pb_rule.generate(0);
+		const NetPortRule::AttrVec np_rules = np_rule.generate(0);
+		CHECK(pb_rules.empty());
+		CHECK(np_rules.empty());
 	}
 }
 
